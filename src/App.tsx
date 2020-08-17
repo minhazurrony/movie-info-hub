b14b9765696a32d107e222370ee369d827c45370
Minhazur Rahman <minhazur.rony@gmail.com>
2020-08-17 13:22:05 +0600

    chore: test api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import { Button } from 'antd';
-import React from 'react';
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return <Button type="primary">Primary Button</Button>;
-}
+const App = () => {
+  const [trendingMovies, setTrendingMovies] = useState<any[]>([]);
+
+  useEffect(() => {
+    const trendingAPI = `https://api.themoviedb.org/3/trending/movie/week?api_key=dbfb4b6d3ceaae796d00053aa80dc1d9`;
+
+    axios
+      .get(trendingAPI)
+      .then((response) => setTrendingMovies(response.data.results));
+  }, []);
+
+  const trendingMovieList = trendingMovies.map((item) => {
+    return (
+      <Fragment key={item.id}>
+        <img
+          key={item.poster_path}
+          src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+          alt="movie_poster"
+        />
+        <li key={item.title}>{item.title}</li>
+      </Fragment>
+    );
+  });
+  return <ul>{trendingMovieList}</ul>;
+};
 
 export default App;
