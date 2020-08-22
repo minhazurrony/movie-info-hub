df76c093c3a28166fb5665f8de1ce9e3c2878206
Minhazur Rahman <minhazur.rony@gmail.com>
2020-08-22 18:40:38 +0600

    refactor: move initial state to app compoent

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,79 @@
 import { Input, Layout } from 'antd';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Loader } from './components/Loader';
 import { TrendingMovies } from './components/TrendingMovies';
-import { TrendingTV } from './components/TrendingTv';
+import { TrendingTvSeries } from './components/TrendingTv';
 import { UpcomingMovies } from './components/UpcomingMovies';
 
 const { Header, Footer, Content } = Layout;
 const { Search } = Input;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [upcomingMovies, setUpcomingMovies] = useState<any[]>([]);
+  const [trendingMovies, setTrendingMovies] = useState<any[]>([]);
+  const [trendingTvSeries, setTrendingTvSeries] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchUpcomingMovies = async () => {
+      const trendingAPI = `https://api.themoviedb.org/3/movie/upcoming?api_key=dbfb4b6d3ceaae796d00053aa80dc1d9&language=en-US&page=1`;
+      setLoading(true);
+      const res = await axios.get(trendingAPI);
+      setUpcomingMovies(res.data.results);
+      setLoading(false);
+    };
+
+    const fetchTrendingMovies = async () => {
+      const trendingAPI = `https://api.themoviedb.org/3/trending/movie/week?api_key=dbfb4b6d3ceaae796d00053aa80dc1d9`;
+      setLoading(true);
+      const res = await axios.get(trendingAPI);
+      setTrendingMovies(res.data.results);
+      setLoading(false);
+    };
+
+    const fetchTrendingTvSeries = async () => {
+      const trendingAPI = `https://api.themoviedb.org/3/trending/tv/week?api_key=dbfb4b6d3ceaae796d00053aa80dc1d9`;
+      setLoading(true);
+      const res = await axios.get(trendingAPI);
+      setTrendingTvSeries(res.data.results);
+      setLoading(false);
+    };
+
+    fetchUpcomingMovies();
+    fetchTrendingMovies();
+    fetchTrendingTvSeries();
+  }, []);
+
   return (
-    <>
-      <Layout>
-        <Header className="app-header">
-          <div className="header-container">
-            <img src={require('./assets/logo.png')} alt="logo" height={30} />
-            <Search
-              style={{ width: 200 }}
-              enterButton
-              placeholder="search movie"
-              onSearch={(value) => console.log(value)}
-            />
-          </div>
-        </Header>
-        <Content className="main-content-container">
-          <UpcomingMovies />
-          <TrendingMovies />
-          <TrendingTV />
-        </Content>
-        <Footer className="app-footer">
-          <p>Build with TMDB</p>
-        </Footer>
-      </Layout>
-    </>
+    <Layout>
+      <Header className="app-header">
+        <div className="header-container">
+          <img src={require('./assets/logo.png')} alt="logo" height={30} />
+          <Search
+            style={{ width: 200 }}
+            enterButton
+            placeholder="search movie"
+            onSearch={(value) => console.log(value)}
+          />
+        </div>
+      </Header>
+      <Content className="main-content-container">
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <UpcomingMovies upcomingMovies={upcomingMovies} />
+            <TrendingMovies trendingMovies={trendingMovies} />
+            <TrendingTvSeries trendingTvSeries={trendingTvSeries} />
+          </>
+        )}
+      </Content>
+      <Footer className="app-footer">
+        <p>Build with TMDB api</p>
+      </Footer>
+    </Layout>
   );
 };
 
--- src/components/UpcomingMovies/index.tsx
+++ src/components/UpcomingMovies/index.tsx
@@ -1,30 +1,18 @@
 import { Col, Pagination, Row } from 'antd';
-import axios from 'axios';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { InfoModal } from '../InfoModal';
-import { Loader } from '../Loader';
 import { MovieCard } from '../MovieCard';
 import './upcomingMovies.css';
 
-export const UpcomingMovies = () => {
-  const [upcomingMovies, setUpcomingMovies] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
+interface UpcomingMovieProps {
+  upcomingMovies: Array<any>;
+}
+
+export const UpcomingMovies = ({ upcomingMovies }: UpcomingMovieProps) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [moviesPerPage] = useState(6);
   const [showMovieDetailsModal, setShowMovieDetailsModal] = useState(false);
   const [selectedItem, setSelectedItem] = useState({});
-
-  useEffect(() => {
-    const fetchUpcomingMovies = async () => {
-      const trendingAPI = `https://api.themoviedb.org/3/movie/upcoming?api_key=dbfb4b6d3ceaae796d00053aa80dc1d9&language=en-US&page=1`;
-      setLoading(true);
-      const res = await axios.get(trendingAPI);
-      setUpcomingMovies(res.data.results);
-      setLoading(false);
-    };
-
-    fetchUpcomingMovies();
-  }, []);
 
   // Get current movie
   const indexOfLastMovie = currentPage * moviesPerPage;
@@ -36,59 +24,53 @@ export const UpcomingMovies = () => {
 
   return (
     <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          <h1 className="upcomingMovieTitle">Coming Soon</h1>
+      <h1 className="upcomingMovieTitle">Coming Soon</h1>
 
-          <Row gutter={[16, 16]}>
-            {currentMovie.map((item) => (
-              <Fragment key={item.id}>
-                <Col
-                  xs={{ span: 12 }}
-                  sm={{ span: 12 }}
-                  md={{ span: 8 }}
-                  lg={{ span: 4 }}
-                  xl={{ span: 4 }}
-                >
-                  <MovieCard
-                    imageSource={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-                    imageAlerText={item.title}
-                    movieTitle={item.title}
-                    releaseYear={item.release_date}
-                    averageRating={item.vote_average}
-                    handleCardClick={() => {
-                      setSelectedItem(item);
-                      setShowMovieDetailsModal(true);
-                    }}
-                  />
-                </Col>
-              </Fragment>
-            ))}
-          </Row>
-
-          <Row>
-            <Col span={24}>
-              <Pagination
-                current={currentPage}
-                defaultCurrent={1}
-                defaultPageSize={moviesPerPage}
-                total={upcomingMovies.length}
-                onChange={(page) => setCurrentPage(page)}
+      <Row gutter={[16, 16]}>
+        {currentMovie.map((item) => (
+          <Fragment key={item.id}>
+            <Col
+              xs={{ span: 12 }}
+              sm={{ span: 12 }}
+              md={{ span: 8 }}
+              lg={{ span: 4 }}
+              xl={{ span: 4 }}
+            >
+              <MovieCard
+                imageSource={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                imageAlerText={item.title}
+                movieTitle={item.title}
+                releaseYear={item.release_date}
+                averageRating={item.vote_average}
+                handleCardClick={() => {
+                  setSelectedItem(item);
+                  setShowMovieDetailsModal(true);
+                }}
               />
             </Col>
-          </Row>
+          </Fragment>
+        ))}
+      </Row>
 
-          <InfoModal
-            isVisible={showMovieDetailsModal}
-            handleOk={(e) => setShowMovieDetailsModal(false)}
-            detailsInfo={selectedItem}
-            handleClose={(e) => setShowMovieDetailsModal(false)}
-            isFromMovie={true}
+      <Row>
+        <Col span={24}>
+          <Pagination
+            current={currentPage}
+            defaultCurrent={1}
+            defaultPageSize={moviesPerPage}
+            total={upcomingMovies.length}
+            onChange={(page) => setCurrentPage(page)}
           />
-        </>
-      )}
+        </Col>
+      </Row>
+
+      <InfoModal
+        isVisible={showMovieDetailsModal}
+        handleOk={(e) => setShowMovieDetailsModal(false)}
+        detailsInfo={selectedItem}
+        handleClose={(e) => setShowMovieDetailsModal(false)}
+        isFromMovie={true}
+      />
     </>
   );
 };
