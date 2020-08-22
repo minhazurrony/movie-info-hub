7e723b06d5d4a82e24dbecb1b0d58839c91f87f5
Minhazur Rahman <minhazur.rony@gmail.com>
2020-08-22 17:17:10 +0600

    chore: update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd';
+import { Input, Layout } from 'antd';
 import React from 'react';
 import './App.css';
 import { TrendingMovies } from './components/TrendingMovies';
@@ -6,21 +6,24 @@ import { TrendingTV } from './components/TrendingTv';
 import { UpcomingMovies } from './components/UpcomingMovies';
 
 const { Header, Footer, Content } = Layout;
+const { Search } = Input;
 
 const App = () => {
   return (
     <>
       <Layout>
-        <Header
-          style={{ position: 'fixed', zIndex: 1, width: '100%' }}
-        ></Header>
-        <Content
-          style={{
-            paddingLeft: '50px',
-            paddingRight: '50px',
-            marginTop: '95px',
-          }}
-        >
+        <Header className="app-header">
+          <div className="header-container">
+            <img src={require('./assets/logo.png')} alt="logo" height={30} />
+            <Search
+              style={{ width: 200 }}
+              enterButton
+              placeholder="search movie"
+              onSearch={(value) => console.log(value)}
+            />
+          </div>
+        </Header>
+        <Content className="main-content-container">
           <UpcomingMovies />
           <TrendingMovies />
           <TrendingTV />
